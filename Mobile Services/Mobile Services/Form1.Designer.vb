593fe50c282ea50ca3f5a1e75c1a37e43444facc
Monde Castro <45668982+Monde-Castro@users.noreply.github.com>
2022-08-04 16:44:00 +0200

    Added files via upload

---
+++ Mobile Services/Mobile Services/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMobileService
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtPayment = New System.Windows.Forms.TextBox()
+        Me.txtMonth = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(83, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(555, 49)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "MOBILE PHONE SERVICES"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(471, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(167, 100)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "012 Slik Road" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Cape Town" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "SOUTH AFRICA" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "110101"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtPayment)
+        Me.GroupBox1.Controls.Add(Me.txtMonth)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(83, 182)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(382, 129)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Enter Data"
+        '
+        'txtPayment
+        '
+        Me.txtPayment.Location = New System.Drawing.Point(139, 81)
+        Me.txtPayment.Name = "txtPayment"
+        Me.txtPayment.Size = New System.Drawing.Size(204, 29)
+        Me.txtPayment.TabIndex = 3
+        '
+        'txtMonth
+        '
+        Me.txtMonth.Location = New System.Drawing.Point(139, 42)
+        Me.txtMonth.Name = "txtMonth"
+        Me.txtMonth.Size = New System.Drawing.Size(204, 29)
+        Me.txtMonth.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 81)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(98, 24)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Payment : "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 24)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Month : "
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(524, 219)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(114, 34)
+        Me.btnAdd.TabIndex = 3
+        Me.btnAdd.Text = "&Add Data"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDisplay.Location = New System.Drawing.Point(524, 263)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(114, 34)
+        Me.btnDisplay.TabIndex = 4
+        Me.btnDisplay.Text = "&Display "
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.ItemHeight = 20
+        Me.lstDisplay.Location = New System.Drawing.Point(83, 317)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(496, 304)
+        Me.lstDisplay.TabIndex = 5
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(585, 467)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(114, 34)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(585, 599)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(114, 34)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMobileService
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(719, 670)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmMobileService"
+        Me.Text = "Mobile Service"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtPayment As System.Windows.Forms.TextBox
+    Friend WithEvents txtMonth As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnDisplay As System.Windows.Forms.Button
+    Friend WithEvents lstDisplay As System.Windows.Forms.ListBox
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Mobile Services/Mobile Services/Form1.vb
@@ -0,0 +1,125 @@
+﻿Public Class frmMobileService
+
+    'Declare arrays
+    Dim decArrPayment() As Decimal
+    Dim strArrMonth() As String
+    Dim intCounter As Integer
+
+    Private Sub frmMobileService_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Prevent Error if arrays are empty
+        btnDisplay.Enabled = False
+    End Sub
+
+    Private Function CalcAverage() As Decimal
+        'Calculate and return the Average Monthly Payment
+        Dim J As Integer
+        Dim decTotal As Decimal = 0
+        Dim decCalcAverage As Decimal = 0
+
+        For J = 0 To UBound(decArrPayment)
+            decTotal = decTotal + decArrPayment(J)
+        Next
+        decCalcAverage = decTotal / intCounter
+
+        Return decCalcAverage
+    End Function
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        'Check for empty Month textbox
+        If txtMonth.Text = " " Then
+            Beep()
+            MessageBox.Show("Enter a Month", "Enter Error ")
+            txtMonth.Focus()
+            Exit Sub
+        End If
+
+        'Re-dimension Arrays every time a new element is added
+        'Preserve keeps data elements that is already in Array
+        ReDim Preserve strArrMonth(intCounter)
+        ReDim Preserve decArrPayment(intCounter)
+
+
+
+        'Assign contents of Month textbox to array variable
+        strArrMonth(intCounter) = txtMonth.Text
+
+        'Check for valid Numeric input
+        If IsNumeric(txtPayment.Text) Then
+            decArrPayment(intCounter) = CDec(txtPayment.Text)
+        Else
+            Beep()
+            MessageBox.Show("Enter a valid payment", "Enter Error")
+            txtPayment.Clear()
+            txtPayment.Focus()
+            Exit Sub
+        End If
+
+        intCounter = intCounter + 1
+
+        'Reset Control properties
+        txtMonth.Clear()
+        txtPayment.Clear()
+        txtMonth.Focus()
+
+        If btnDisplay.Enabled = False Then
+            btnDisplay.Enabled = True
+        End If
+
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        'Display data in Listbox
+        'Calculate the Average Monthly Payment
+        'Display the Monthly Payments above the Average Payment
+
+        Dim J As Integer
+        Dim decAverage As Decimal
+
+        lstDisplay.Items.Clear()
+        lstDisplay.Items.Add("")
+        lstDisplay.Items.Add("Month".PadRight(20) & "Payment".PadLeft(10))
+
+        lstDisplay.Items.Add("")
+
+        For J = 0 To UBound(strArrMonth)
+            lstDisplay.Items.Add(strArrMonth(J).PadRight(20) &
+            decArrPayment(J).ToString("C2").PadLeft(10))
+        Next
+
+        'Call Function to calculate Average Monthly Payment
+        decAverage = CalcAverage()
+
+        'Display Months and Payments greater than the Average Payment in the listbox
+        lstDisplay.Items.Add("")
+        lstDisplay.Items.Add("Average Monthly Payments : ".PadRight(20) &
+                             decAverage.ToString("C2").PadLeft(10))
+        lstDisplay.Items.Add("")
+        lstDisplay.Items.Add("Month(s) with an above Average Payment(s) : ")
+        lstDisplay.Items.Add("")
+
+        'Display Months And Paymentd greater then Average Payments in the listbox
+
+        For J = 0 To UBound(strArrMonth)
+            If decArrPayment(J) > decAverage Then
+                lstDisplay.Items.Add(strArrMonth(J).PadRight(20) &
+                                     decArrPayment(J).ToString("C2").PadLeft(10))
+            End If
+        Next
+
+
+        'Set focus to Month textbox
+        txtMonth.Focus()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear the listbox and set focus to the month textbox
+        lstDisplay.Items.Clear()
+        txtMonth.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form and terminate the program
+        Close()
+
+    End Sub
+End Class
